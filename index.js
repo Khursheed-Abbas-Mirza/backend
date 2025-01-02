8176c17c96ce19c10d8e31af0f66d4da19a3e93e
Khursheed Abbas <mirzakhursheedabbas14@gmail.com>
2025-01-02 13:44:25 +0530

    home added by me

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app=express()
 const bodyparser=require('body-parser')
 const bookroute=require('./Routes/booking')
 const cors=require('cors')
+app.use(express.static('public'));
 app.use(cors())
 app.use(bodyparser.json())
 app.use('/api',bookroute)
 app.get('/',(req,res)=>{
-    res.send("Hello world")
+    res.sendFile(__dirname + '/views/index.html');
 })
+
 app.listen(5000,()=>{
     console.log("App listening on the port number 5000")
 })
