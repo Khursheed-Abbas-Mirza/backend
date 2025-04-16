cda88ee5e3e90a381545a759df3d04a3c94cbc71
Khursheed Abbas <mirzakhursheedabbas14@gmail.com>
2025-04-16 18:04:01 +0530

    added assets

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const bodyparser=require('body-parser')
 const bookroute=require('./Routes/booking')
 const cors=require('cors')
 app.set("view engine","ejs")
-app.use("/",express.static('public'));
+app.use("/asset",express.static('public'));
 app.use(cors())
 app.use(bodyparser.json())
 app.use('/api',bookroute)
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+"/views"+"/index.html")
 })
-// app.listen(5000,()=>{console.log("Serever started")})
+app.listen(5000,()=>{console.log("Serever started")})
 module.exports=app
