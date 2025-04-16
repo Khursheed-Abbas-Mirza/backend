1cf71b86e88839e02beafe70f74e37774470bfa1
Khursheed Abbas <mirzakhursheedabbas14@gmail.com>
2025-04-16 16:59:44 +0530

    done by me

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const app=express()
 const bodyparser=require('body-parser')
 const bookroute=require('./Routes/booking')
 const cors=require('cors')
+app.set("view engine","ejs")
 app.use(express.static('public'));
 app.use(cors())
 app.use(bodyparser.json())
 app.use('/api',bookroute)
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/views/index.html');
+    res.render("index");
 })
 
-app.listen(5000,()=>{
-    console.log("App listening on the port number 5000")
-})
+module.export=app
